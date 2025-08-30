a06aaa564da65a96607e7c2f10e67c102bd5f0f8
Shanto <husain.imambd@gmail.com>
2025-08-30 22:08:32 +0200

    07 loop for user continuous input

--- 07_Hangman_game/Main_hangman.py
+++ 07_Hangman_game/Main_hangman.py
@@ -11,14 +11,15 @@ for space in range(wordlen):
     placeholder.append("_")
 placeholderStr = ''.join(placeholder)
 print(placeholderStr)   #print out the placeholder
+# loop for guess till game is finished
+while "_" in placeholder:
+    guess = input("Guess a letter:  ").lower() # lower case the guess to avoid complication
+    index = 0
+    for letter in chosen_word:
 
-guess = input("Guess a letter:  ").lower() # lower case the guess to avoid complication
-index = 0
-for letter in chosen_word:
+        if letter == guess:
+            placeholder[index] = guess
+        index += 1
 
-    if letter == guess:
-        placeholder[index] = guess
-    index += 1
-
-placeholderStr = ''.join(placeholder)
-print(placeholderStr)
+    placeholderStr = ''.join(placeholder)
+    print(placeholderStr)
