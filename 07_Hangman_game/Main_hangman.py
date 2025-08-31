75078ac3d116ca86a114389d0249c46bd0e89e07
Shanto <husain.imambd@gmail.com>
2025-08-31 11:51:40 +0200

    07 winnner logic

--- 07_Hangman_game/Main_hangman.py
+++ 07_Hangman_game/Main_hangman.py
@@ -1,7 +1,9 @@
 word_list = ["aardvark", "baboon", "camel"]
 
 import random
+import Hangman_asciiArt
 word_list = ["aardvark", "baboon", "camel"]
+lives = 6 # number of choices a player will have
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -13,15 +15,25 @@ for position in range(word_length):
     placeholder.append("_")
 display = "".join(placeholder)
 print(display)
-while "_" in placeholder:
+game_over = False
+while not game_over and lives > 0 :
     guess = input("Guess a letter: ").lower()
 
-    index = 0
 
+    if guess in chosen_word:
+        for index,letter in enumerate(chosen_word) :
+            if letter == guess:
+                placeholder[index] = letter
+    else:
+        lives -= 1
+        print(f"Wrong guess! Lives left: {lives}")
+        print(Hangman_asciiArt.stages[lives])
 
-    for letter in chosen_word:
-        if letter == guess:
-            placeholder[index] = letter
-        index += 1
     display = "".join(placeholder)
     print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You win")
+if lives == 0 and "_" in placeholder:
+    print("You lose! The word was:", chosen_word)
