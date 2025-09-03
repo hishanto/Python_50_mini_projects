cc30d638d1f5e90ebcfc7fb51e2fa258bfe8edf8
Shanto <husain.imambd@gmail.com>
2025-09-03 19:00:20 +0200

    08_alphabet and user input

--- 08_Ceaser_cipher/Main_ceaseCipher.py
+++ 08_Ceaser_cipher/Main_ceaseCipher.py
@@ -1,6 +1,5 @@
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-greet_with_name("Jack Bauer")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
