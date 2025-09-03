b80928075fe4bb481f80ccd1cbbc56232e0c17ba
Shanto <husain.imambd@gmail.com>
2025-09-03 19:37:20 +0200

    08_encode and decode function

--- 08_Ceaser_cipher/Main_ceaseCipher.py
+++ 08_Ceaser_cipher/Main_ceaseCipher.py
@@ -3,3 +3,28 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(cipher_text)
+
+encrypt(text,shift)
+
+def decrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        #shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(cipher_text)
+
+def ceaser(direction, text, shift):
+    if direction == 'encode':
+        encrypt(text, shift)
+    elif direction == "decode":
+        decrypt(text,shift)
+
+ceaser(direction, text, shift)
