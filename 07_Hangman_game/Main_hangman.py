25f56c591b29b48128652931512d2722733f9c62
Shanto <husain.imambd@gmail.com>
2025-09-03 15:19:52 +0200

    07 Hangman Ascii user experince

--- 07_Hangman_game/Main_hangman.py
+++ 07_Hangman_game/Main_hangman.py
@@ -1,13 +1,13 @@
-word_list = ["aardvark", "baboon", "camel"]
 
+import Hangman_words
 import random
 import Hangman_asciiArt
 
 print(Hangman_asciiArt.logo)
-word_list = ["aardvark", "baboon", "camel"]
+
 lives = 6 # number of choices a player will have
 
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(Hangman_words.word_list)
 print(chosen_word)
 
 placeholder = []
@@ -19,16 +19,22 @@ display = "".join(placeholder)
 print(display)
 game_over = False
 while not game_over and lives > 0 :
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
 
+    if guess in placeholder:
+        print(f"You already guessed: {guess}")
 
-    if guess in chosen_word:
+    elif guess in chosen_word:
         for index,letter in enumerate(chosen_word) :
             if letter == guess:
                 placeholder[index] = letter
+        print(f"Good guess!")
+        print(Hangman_asciiArt.stages[lives])
+
     else:
         lives -= 1
-        print(f"Wrong guess! Lives left: {lives}")
+        print(f"Wrong guess!\n")
         print(Hangman_asciiArt.stages[lives])
 
     display = "".join(placeholder)
@@ -36,6 +42,6 @@ while not game_over and lives > 0 :
 
     if "_" not in display:
         game_over = True
-        print("You win")
+        print("****************************You win****************************")
 if lives == 0 and "_" in placeholder:
-    print("You lose! The word was:", chosen_word)
+    print("You lose! The word was: ", chosen_word)
