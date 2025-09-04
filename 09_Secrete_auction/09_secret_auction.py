7daf01173815d74a6321f2781dbdcf35e046d3b8
Shanto <husain.imambd@gmail.com>
2025-09-04 19:55:59 +0200

    09_Secret_auction_ input loop till plaer says no

--- 09_Secrete_auction/09_secret_auction.py
+++ 09_Secrete_auction/09_secret_auction.py
@@ -2,8 +2,21 @@ import art_secret_bid
 print(art_secret_bid.logo)
 UserName = ""
 bid = 0
+newUser = "yes"
+ContinueInput = True
+storage_dict = {
+}
 
+# Get input from the user
 def userInput():
     UserName = input("What is your name: ")
     bid = int(input("Pleas write your bid: "))
-userInput()
+    storage_dict[UserName] = bid
+
+#Continue input from the player
+while ContinueInput == True:
+    userInput()
+    newUser = input("Is there any player left ?\nWrite 'yes' or 'no' : ").lower()
+    print("\n" * 100)
+    if newUser == "no":
+        ContinueInput = False
