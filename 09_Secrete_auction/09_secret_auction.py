33c0775c429e263289bda13737a48742760e6fc8
Shanto <husain.imambd@gmail.com>
2025-09-04 16:12:08 +0200

    09_Scret_auction_gavel

--- 09_Secrete_auction/09_secret_auction.py
+++ 09_Secrete_auction/09_secret_auction.py
@@ -1,14 +1,2 @@
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.",
-    "Function": "A piece of code that you can easily call over and over again.",
-}
-print(programming_dictionary["Bug"])
-
-programming_dictionary["Loop"] = " The action is doing over and over again"
-
-print(programming_dictionary)
-
-empty_dictionalry = {}
-
-nested_list = ["a", "b", [ "c", "d"]]
-print(nested_list[2][1])
+import art_secret_bid
+print(art_secret_bid.logo)
