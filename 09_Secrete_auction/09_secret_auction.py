e403a5b20bc1c535b771c9ba8fc0a3d8bf39a2bf
Shanto <husain.imambd@gmail.com>
2025-09-04 15:04:57 +0200

    09_Scret_auction

---
+++ 09_Secrete_auction/09_secret_auction.py
@@ -0,0 +1,14 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+print(programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = " The action is doing over and over again"
+
+print(programming_dictionary)
+
+empty_dictionalry = {}
+
+nested_list = ["a", "b", [ "c", "d"]]
+print(nested_list[2][1])
