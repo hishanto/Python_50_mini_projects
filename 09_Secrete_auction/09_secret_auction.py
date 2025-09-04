9ffede81067ccfa7393daee259e132e0564d7fef
Shanto <husain.imambd@gmail.com>
2025-09-04 16:19:02 +0200

    09_Secret_auction_user input

--- 09_Secrete_auction/09_secret_auction.py
+++ 09_Secrete_auction/09_secret_auction.py
@@ -1,2 +1,9 @@
 import art_secret_bid
 print(art_secret_bid.logo)
+UserName = ""
+bid = 0
+
+def userInput():
+    UserName = input("What is your name: ")
+    bid = int(input("Pleas write your bid: "))
+userInput()
