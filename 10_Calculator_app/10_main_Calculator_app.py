e7db42d6a1323e248923848f4a7b6a95bd6379ab
Shanto <husain.imambd@gmail.com>
2025-09-10 18:57:38 +0200

    10_Calculator_ all operation fuctional

--- 10_Calculator_app/10_main_Calculator_app.py
+++ 10_Calculator_app/10_main_Calculator_app.py
@@ -1,8 +1,19 @@
 def add(n1, n2):
     return n1 + n2
-def sub(n1, n2):
+def subtract(n1, n2):
     return n1 - n2
-def mul(n1, n2):
+def multiply (n1, n2):
     return n1 * n2
-def div(n1, n2):
+def divide(n1, n2):
     return n1 / n2
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+}
+firstVar = int(input("Enter the 1st Number: "))
+userIn = input("Enter the operation you want to execute")
+lastVar = int(input("Enter the 2nd Number: "))
+result = operations[userIn](firstVar, lastVar)
+print(result)
