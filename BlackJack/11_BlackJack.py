2fc0b8447fbe353a49c4b16f5c05cb7905cc3b1b
Shanto <husain.imambd@gmail.com>
2025-09-25 21:00:07 +0200

    11_BlackJack_ Assign new cards to the players

--- BlackJack/11_BlackJack.py
+++ BlackJack/11_BlackJack.py
@@ -1,3 +1,18 @@
 import Black_jack_art
+import random
+
 print(Black_jack_art.logo)
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+user_cards = []
+computer_cards = []
+
+def deal_card():
+    """ Returns a random card from the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+for _ in range(2):
+    '''Assign new initial cards to players'''
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
